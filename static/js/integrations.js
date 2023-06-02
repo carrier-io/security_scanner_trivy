f8f7eb13174a90a4a3e831346954f621bf42e840
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-06-02 22:38:39 +0500

    Adapted to new integration api and supports modes

--- static/js/integrations.js
+++ static/js/integrations.js
@@ -9,7 +9,7 @@ const trivyIntegration = {
         class="modal modal-small fixed-left fade shadow-sm" tabindex="-1" role="dialog"
     >
         <ModalDialog
-            v-model:description="description"
+            v-model:name="config.name"
             v-model:is_default="is_default"
             @update="update"
             @create="create"
@@ -96,7 +96,7 @@ const trivyIntegration = {
         </template>
         <template #footer>
             <test-connection-button
-                    :apiPath="api_base + 'check_settings'"
+                    :apiPath="this.$root.build_api_url('integrations', 'check_settings') + '/' + pluginName"
                     :error="error.check_connection"
                     :body_data="body_data"
                     v-model:is_fetching="is_fetching"
@@ -116,15 +116,12 @@ const trivyIntegration = {
         })
     },
     computed: {
-        apiPath() {
-            return this.api_base + 'integration/'
-        },
         project_id() {
             return getSelectedProjectId()
         },
         body_data() {
             const {
-                description,
+                config,
                 is_default,
                 project_id,
 
@@ -137,9 +134,10 @@ const trivyIntegration = {
                 trivy_options,
 
                 status,
+                mode,
             } = this
             return {
-                description,
+                config,
                 is_default,
                 project_id,
 
@@ -151,6 +149,7 @@ const trivyIntegration = {
                 show_without_description,
                 trivy_options,
                 status,
+                mode,
             }
         },
         modal() {
@@ -176,17 +175,21 @@ const trivyIntegration = {
         },
         handleEdit(data) {
             console.debug('trivy editIntegration', data)
-            const {description, is_default, id, settings} = data
-            this.load({...settings, description, is_default, id})
+            const {config, is_default, id, settings} = data
+            this.load({...settings, config, is_default, id})
             this.modal.modal('show')
         },
         handleDelete(id) {
             this.load({id})
             this.delete()
         },
+        handleSetDefault(id) {
+            this.load({id})
+            this.set_default()
+        },
         create() {
             this.is_fetching = true
-            fetch(this.apiPath + this.pluginName, {
+            fetch(this.api_url + this.pluginName, {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(this.body_data)
@@ -216,7 +219,7 @@ const trivyIntegration = {
         },
         update() {
             this.is_fetching = true
-            fetch(this.apiPath + this.id, {
+            fetch(this.api_url + this.id, {
                 method: 'PUT',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(this.body_data)
@@ -232,7 +235,7 @@ const trivyIntegration = {
         },
         delete() {
             this.is_fetching = true
-            fetch(this.apiPath + this.id, {
+            fetch(this.api_url + this.project_id + '/' + this.id, {
                 method: 'DELETE',
             }).then(response => {
                 this.is_fetching = false
@@ -245,8 +248,29 @@ const trivyIntegration = {
                 }
             })
         },
+        async set_default() {
+            console.log('we are here')
+            this.is_fetching = true
+            try {
+                const resp = await fetch(this.api_url + this.id, {
+                    method: 'PATCH',
+                })
+                if (resp.ok) {
+                    this.$emit('update', {...this.$data, section_name: this.section_name})
+                } else {
+                    const error_data = await resp.json()
+                    this.handleError(error_data)
+                }
+            } catch (e) {
+                console.error(e)
+                showNotify('ERROR', 'Error setting as default')
+            } finally {
+                this.is_fetching = false
+            }
+        },
+
         initialState: () => ({
-            description: '',
+            config: {},
             is_default: false,
             is_fetching: false,
             error: {},
@@ -262,8 +286,9 @@ const trivyIntegration = {
             trivy_options: "--no-progress",
 
             pluginName: 'security_scanner_trivy',
-            api_base: '/api/v1/integrations/',
+            api_url: V.build_api_url('integrations', 'integration') + '/',
             status: integration_status.success,
+            mode: V.mode
         }),
     }
 }
